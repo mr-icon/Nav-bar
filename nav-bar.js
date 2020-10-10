3c4e3cc49ecef56050699ba2e19bc5cb0a7b0426
mr-icon <emmanuelaustin116@gamil.com>
2020-10-09 23:23:03 -0700

    pushing back update

--- nav-bar.js
+++ nav-bar.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
     // insert code here
+    var open = false
     $("button").click(function(){
-        if($("button").text() == "-"){
-            $("button").text("X");
-        }else{
-            $("button").text("-");
+        open = !open
+        if(open){
+            document.getElementById('code').style.display = 'none'
+            document.getElementById('close').style.display = 'inline'
+            document.getElementById('face').style.display = 'none'
+            $("ul").toggle("slow")
         }
-        $("ul").toggle("slow")
+        else{
+            document.getElementById('close').style.display = 'none'
+            document.getElementById('face').style.display = 'inline'
+            setTimeout(()=>{
+                document.getElementById('code').style.display = 'inline'
+            },500)
+            
+            $("ul").toggle("slow")
+        }
+        
     })
 })
